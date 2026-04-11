b6dffbc2d8c903825dfc0fdcf1c8a3ca6749eeef
Pathompum Jirakarnpaisan <107536914+Saannddy@users.noreply.github.com>
2026-04-12 04:21:34 +0700

    refactor: refactor the whole project to be more clean

--- build_macos.sh
+++ build_macos.sh
@@ -2,5 +2,5 @@
 rm -rf build/ dist/ *.spec
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-pyinstaller --onedir --windowed --name "おはよう" --icon=icon.png app.py
+pyinstaller --onedir --windowed --name "おはよう" --icon=images/icon.png app.py
 echo "✅ macOS app created: dist/おはよう.app"
